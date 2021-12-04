13e911ef0531194b0a399ac4096935ef863586f3
picdotjpg <76465616+picdotjpg@users.noreply.github.com>
2021-12-04 06:15:17 -0800

    add refresh (test)

--- refreshdir.sh
+++ refreshdir.sh
@@ -1,25 +1,8 @@
-
-sudo mkdir /www/
-sudo mkdir /www/vpns/
-sudo chmod 645 /www/vpns/
-
-
-#installs busybox for httpd
-apt-get update
-apt-get install -y busybox
-apt-get install -y python
-/bin/busybox httpd -h /www/vpns/ -p 80
-echo "[Unit]
-Description=Simple busybox httpd service
-Before=network-online.target
-Wants=network-online.target
-[Service]
-Type=oneshot
-ExecStart=/bin/busybox httpd -h /www/vpns/ -p 80 && /www/refreshdir.sh
-ExecStop=/bin/busybox httpd -h /www/vpns/ -p 80 && /www/refreshdir.sh
-RemainAfterExit=yes
-[Install]
-WantedBy=multi-user.target" >/etc/systemd/system/vpnshttpd.service
+mv /www/vpns/*.ovpn /www/
+mv /www/vpns/client0.ovpn /www/
+sudo cp $HOME/client0.ovpn /www/
+sudo cp /root/*.ovpn /www/
+rm -rf /www/vpns/*
 sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/index.html -P /www/vpns/
 sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/lin1.png -P /www/vpns/
 sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/lin2.png -P /www/vpns/
@@ -34,28 +17,13 @@ sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/m
 sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/win7.PNG -P /www/vpns/
 sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/win.png -P /www/vpns/
 sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/tux.png -P /www/vpns/
-sudo tar -xvzf img.tar.gz -C /www/vpns/
-sudo mv index.html /www/vpns/
-sudo mv /www/vpns/img/* /www/vpns/
-sudo cp $HOME/*.vpn /www/vpns/
-sudo cp $HOME/*.png /www/vpns/
-sudo wget https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh
 sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/LICENSE -P /www/vpns/
 sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/Default-Free-Vpn-Servers.txt -P /www/vpns/
 sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/Other-Vpn-Servers.txt -P /www/vpns/
-sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/refreshdir.sh -P /www/vpns/
-sudo chmod +x openvpn-install.sh
-export AUTO_INSTALL=y
-export APPROVE_INSTALL=y
-export APPROVE_IP=y
-export IPV6_SUPPORT=n
-export PORT_CHOICE=1
-export PROTOCOL_CHOICE=1
-export DNS=11
-export COMPRESSION_ENABLED=n
-export CUSTOMIZE_ENC=n
-export CLIENT=client0
-export PASS=1
-sudo AUTO_INSTALL=y CLIENT=client0 ./openvpn-install.sh
-sudo cp /root/*.vpn /www/vpns/
-sudo cp $HOME/client0.ovpn /www/vpns/
+sudo rm $HOME/setupvpn.sh
+sudo rm $HOME/refreshdir.sh
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/setupvpn.sh -P $HOME/
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/refreshdir.sh -P $HOME/
+sudo mv /www/*.ovpn /www/vpns/
+sudo cp $HOME/refreshdir.sh /www/refreshdir.sh
+sudo mv /www/client0.ovpn /www/vpns/
