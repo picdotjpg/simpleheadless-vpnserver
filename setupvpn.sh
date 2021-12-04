36800f1dab73a3b70670ff07d060c255eecbc464
picdotjpg <76465616+picdotjpg@users.noreply.github.com>
2021-12-04 05:02:14 -0800

    Update setupvpn.sh

--- setupvpn.sh
+++ setupvpn.sh
@@ -21,11 +21,24 @@ RemainAfterExit=yes
 [Install]
 WantedBy=multi-user.target" >/etc/systemd/system/vpnshttpd.service
 sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/index.html
-sudo wget https://download1506.mediafire.com/1u9266lfydpg/wpvstmccb3zhy04/img.tar.gz
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/lin1.png
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/lin2.png
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/lin3.png
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/lin4.png
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/win1.PNG
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/win2.PNG
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/win3.PNG
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/win4.PNG
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/win5.PNG
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/win6.PNG
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/win7.PNG
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/win.png
+sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/tux.png
 sudo tar -xvzf img.tar.gz -C /www/vpns/
 sudo mv index.html /www/vpns/
 sudo mv /www/vpns/img/* /www/vpns/
 sudo cp ~/*.vpn /www/vpns/
+sudo cp ~/*.png /www/vpns/
 sudo wget https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh
 sudo wget https://raw.githubusercontent.com/picdotjpg/simpleheadless-vpnserver/master/LICENSE -P /www/vpns/
 sudo chmod +x openvpn-install.sh
